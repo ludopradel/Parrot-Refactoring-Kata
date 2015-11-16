c4633dec97714456bf726c813d3b2e7218ac6959
ludopradel <ludovic.pradel@gmail.com>
2015-11-16 19:43:34 +0100

    create African European & Norwegian Parrot classes

---
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,11 @@
+package parrot;
+
+public class AfricanParrot extends Parrot
+{
+
+	public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed)
+	{
+		super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, voltage, isNailed);
+	}
+
+}
--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -8,43 +8,43 @@ public class ParrotTest {
 
 	@Test
     public void getSpeedOfEuropeanParrot() {
-		Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+		Parrot parrot = new EuropeanParrot(0, 0, false);
 		assertThat(parrot.getSpeed()).isEqualTo(12.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_One_Coconut() {
-		Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+		Parrot parrot = new AfricanParrot(1, 0, false);
 		assertThat(parrot.getSpeed()).isEqualTo(3.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
-		Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+		Parrot parrot = new AfricanParrot(2, 0, false);
 		assertThat(parrot.getSpeed()).isEqualTo(0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_No_Coconuts() {
-		Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false);
+		Parrot parrot = new AfricanParrot(0, 0, false);
 		assertThat(parrot.getSpeed()).isEqualTo(12.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_nailed() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true);
+		Parrot parrot = new NorwegianBlueParrot(0, 0, true);
 		assertThat(parrot.getSpeed()).isEqualTo(0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed() {
-		Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false);
+		Parrot parrot = new NorwegianBlueParrot(0, 1.5, false);
 		assertThat(parrot.getSpeed()).isEqualTo(18.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
-		Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
+		Parrot parrot = new NorwegianBlueParrot(0, 4, false);
 		assertThat(parrot.getSpeed()).isEqualTo(24.0);
     }
 }
